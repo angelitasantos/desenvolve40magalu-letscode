8e374f246d20d1fa893f0fb177c6e9e30a0515d3
Angelita Santos <65367322+angelitasantos@users.noreply.github.com>
2022-01-19 22:08:34 -0300

    solutions exercises in python while and for

--- 003-malhas de repetição-while-for/while/aula003-exercicios009.py
+++ 003-malhas de repetição-while-for/while/aula003-exercicios009.py
@@ -7,7 +7,19 @@ variável para a soma; e uma variável para os termos. Lembre-se de que
 4! = 4*3*2*1 que também é igual a 4*3!.
 '''
 
-
 '''
 1/1! + 1/2! + 1/3! + ... = 1.718
 '''
+
+soma = 0
+fatorial = 1
+contador = 1
+
+while contador <= 1000:
+    # fatorial *= contador
+    fatorial = fatorial * contador
+    termo = 1 / fatorial
+    soma = soma + termo
+    contador =  contador + 1
+
+print(soma)
