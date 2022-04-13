94de039ab531826896eee87ca51d39f1a0ee018e
Angelita Santos <65367322+angelitasantos@users.noreply.github.com>
2022-04-13 15:47:09 -0300

    Delete 001-variáveis, entradas e saídas, operadores matematicos directory

--- 001-variáveis, entradas e saídas, operadores matematicos/aula001-exercicios001.py
+++
@@ -1,7 +0,0 @@
-'''
-Questão #1
-Faça um programa que mostre a mensagem "Olá, mundo!" na tela.
-'''
-
-mensagem = 'Olá mundo'
-print(mensagem)
--- 001-variáveis, entradas e saídas, operadores matematicos/aula001-exercicios002.py
+++
@@ -1,7 +0,0 @@
-'''
-Questão #2
-Faça um programa que peça um número e mostre a mensagem "O número informado foi [número]".
-'''
-
-numero_informado = float(input('Digite um numero: '))
-print('O numero informado foi', numero_informado)
